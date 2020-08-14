2c3c20179b9ee42e7f76030c19d6345d28f81f02
易烨 <yiye@yiyedeMacBook-Pro.local>
2020-08-15 03:52:26 +0800

    添加 xss 库防止xss 攻击

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,14 +1,16 @@
+
+const xss = require('xss')
 const { exec, escape } = require('../database/mysql')
 
 const getList = (author, keyword) => {
-    // 通过 escape 防止 sql 攻击
-    author = escape(author)
+    // 通过 xss 库防止xss 攻击、escape 防止 sql 攻击
+    author = xss(escape(author))
     let sql = `select * from blogs where 1=1 `
     if (author) {
         sql += `and author=${ author } `
     }
     if (keyword) {
-        sql += `and title like '%${ escape(keyword) }%' `
+        sql += `and title like '%${ xss(escape(keyword)) }%' `
     }
     sql += `order by createtime desc`
     console.log(sql)
@@ -24,29 +26,28 @@ const getDetail = id => {
 
 const newBlog = (blogData = {}) => {
     // blogData是一个博客对象，包含title、content属性
-    // 通过 escape 防止 sql 攻击
-    const title = escape(blogData.title)
-    const author = escape(blogData.author)
-    const content = escape(blogData.content)
+    // 通过 xss 库防止xss 攻击、escape 防止 sql 攻击
+    const title = xss(escape(blogData.title))
+    const author = xss(escape(blogData.author))
+    const content = xss(escape(blogData.content))
     const createTime = Date.now()
     let sql = `insert into blogs (title, content, createtime, author) values (${ title }, ${ content }, ${ createTime }, ${ author })`
+    console.log(sql)
     return exec(sql)
 }
 
 const updateBlog = (id, blogData = {}) => {
     // id 就是更新博客的 id
     // blogData是一个博客对象，包含title、content属性
-    // 通过 escape 防止 sql 攻击
-    const title = escape(blogData.title)
-    const content = escape(blogData.content)
+    // 通过 xss 库防止xss 攻击、escape 防止 sql 攻击
+    const title = xss(escape(blogData.title))
+    const content = xss(escape(blogData.content))
     const sql = `update blogs set title=${ title }, content=${ content } where id='${ id }'`
     return exec(sql)
 }
 
 const deleteBlog = (id, author) => {
     // id 就是删除博客的 id
-    // 通过 escape 防止 sql 攻击
-    author = escape(author)
     const sql = `delete from blogs where id='${ id }' and author=${ author }`
     return exec(sql)
 }
--- src/controller/user.js
+++ src/controller/user.js
@@ -1,9 +1,10 @@
 
+const xss = require('xss')
 const { exec, escape } = require('../database/mysql')
 const login = (username, password) => {
-    // 通过 escape 防止 sql 攻击
-    username = escape(username)
-    password = escape(password)
+    // 通过 xss 库防止xss 攻击、escape 防止 sql 攻击
+    username = xss(escape(username))
+    password = xss(escape(password))
     const sql = `select username, realname from users where username=${ username } and password=${ password }`
     return exec(sql).then(res => {
         return res[0] || {}
